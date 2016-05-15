cc3db3e5af95b16295cae11e27505f13b1ce0910
Jim Small <smallj@live.com>
2016-05-15 13:27:31 -0400

    Initial checkin

---
+++ TheBirds/Birds.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBirds
+{
+    class World
+    {
+        public static int tempurature = 0;
+        public static int windSpeed = 0;
+    }
+
+    abstract class Birds : World
+    {
+        protected int distanceTravled { get; set; }
+
+        public virtual void hop()
+        {
+            throw new ApplicationException("don't hop!");
+        }
+
+        public virtual void fly()
+        {
+            throw new ApplicationException("can't fly!");
+        }
+
+        public Birds ()
+        {
+            distanceTravled = 0;
+        }
+
+        public int getDistanceTravled
+        {
+            get { return distanceTravled; }
+        }
+    }
+
+    class Penguin : Birds
+    {
+        public override void hop()
+        {
+            distanceTravled += 2;
+        }
+        
+        public override void fly()
+        {
+            try
+            {
+                base.fly();
+            }
+            catch (Exception e)
+            {
+                String msg = "Penguins " + e;
+            }
+        }
+    }
+
+    class Hawk : Birds
+    {
+        public override void hop()
+        {
+            try
+            {
+                base.hop();
+            }
+            catch (Exception e)
+            {
+                String msg = "Hawks " + e;
+            }
+        }
+
+        public override void fly ()
+        {
+            if (windSpeed <= 40)
+                distanceTravled += 100;
+        }
+    }
+
+    class Robin : Birds
+    {
+        public override void hop()
+        {
+            if (tempurature >= 0)
+                distanceTravled += 1;
+        }
+
+        public override void fly()
+        {
+            if (windSpeed <= 20)
+                distanceTravled += 20;
+        }
+    }
+
+    class Crow : Birds
+    {
+        public override void hop()
+        {
+            if (tempurature >= 0)
+                distanceTravled += 1;
+        }
+
+        public override void fly()
+        {
+            if (windSpeed <= 25)
+                distanceTravled += 30;
+        }
+    }
+}
---
+++ TheBirds/MainWindow.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TheBirds
+{
+    public partial class MainWindow
+    {
+        private void moveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Penguin penguin = new Penguin();
+            Hawk hawk = new Hawk();
+            Robin robin = new Robin();
+            Crow crow = new Crow();
+
+            //Start world
+            World.tempurature = 20;
+            World.windSpeed = 12;
+
+            penguin.hop();
+            penguin.fly();
+
+            hawk.hop();
+            hawk.fly();
+
+            robin.hop();
+            robin.fly();
+
+            crow.hop();
+            crow.fly();
+
+            //Storm
+            World.tempurature = -10;
+            World.windSpeed = 30;
+
+            penguin.hop();
+
+            hawk.hop();
+            hawk.fly();
+
+            robin.hop();
+            robin.fly();
+
+            crow.hop();
+            crow.fly();
+
+            //Print distances
+            result.Items.Add(String.Format("The penguin moved {0} ft.", penguin.getDistanceTravled));
+            result.Items.Add(String.Format("The hawk moved {0} ft.", hawk.getDistanceTravled));
+            result.Items.Add(String.Format("The robin moved {0} ft.", robin.getDistanceTravled));
+            result.Items.Add(String.Format("The crow moved {0} ft.", crow.getDistanceTravled));
+        }
+    }
+}
